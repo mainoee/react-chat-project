aabede8fd2d6fa44aa0373a83881065008b78e6a
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-27 00:11:23 +0200

    create firebase file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAKE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
