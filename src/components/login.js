81833919b34fddacde1fcc6997c372d641d1e7ca
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-27 10:24:47 +0200

    add form to Login Page with username and password fields, and add CSS style

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 
 class Login extends Component {
-  render() {
+render() {
     return (
-        <div>
-            <h3>React Login Component</h3>
-        </div>
+        <form>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+            <label>Username</label>
+            <input type="username" className="form-control" placeholder="Enter username" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Enter password" />
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-block">Submit</button>
+        </form>
     );
   }
 }
