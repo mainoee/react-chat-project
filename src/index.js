7716b6619caaeb26012770bb94e73341ba2aa58f
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-28 16:17:42 +0200

    update index with Firebase.Provider

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import './assets/stylesheets/authentication.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/app';
+import Firebase, { FirebaseContext } from './service';
 
 ReactDOM.render(
-  <App />,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
