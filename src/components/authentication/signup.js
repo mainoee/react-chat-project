76629a3a6faada0c245b93179e24487c21a2d20b
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-28 17:11:39 +0200

    correct error in redirection path in Signup page

--- src/components/authentication/signup.js
+++ src/components/authentication/signup.js
@@ -30,7 +30,7 @@ class SignUpFormBase extends Component {
       .doCreateUserWithEmailAndPassword(email, password)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
-        this.props.history.push(/home);
+        this.props.history.push('/home');
       })
       .catch(error => {
         this.setState({ error });
