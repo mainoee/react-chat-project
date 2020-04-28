e80295f0a5d8f90be127f64efe5f203d30645d99
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-28 19:24:54 +0200

    update Login page with Firebase authentication

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,24 +1,86 @@
 import React, { Component } from "react";
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-class Login extends Component {
-render() {
+import { withFirebase } from '../../service';
+
+const Login = () => (
+  <div>
+    <LoginForm />
+  </div>
+);
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
+class LoginFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { email, password } = this.state;
+
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push('/home');
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = email === '' || password === '';
+
     return (
       <div className="auth-wrapper">
         <div className="auth-inner">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <h3>Login</h3>
 
             <div className="form-group">
-              <label>Username</label>
-              <input type="username" className="form-control" placeholder="Enter username" />
+              <label>Your email</label>
+              <input
+                name="email"
+                value={email}
+                onChange={this.onChange}
+                type="text"
+                className="form-control"
+                placeholder="Enter email"
+              />
             </div>
 
             <div className="form-group">
               <label>Password</label>
-              <input type="password" className="form-control" placeholder="Enter password" />
+              <input
+                name="password"
+                value={password}
+                onChange={this.onChange}
+                type="password"
+                className="form-control"
+                placeholder="Enter password" />
             </div>
 
-            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            <button disabled={isInvalid} type="submit" className="btn btn-primary btn-block">Login</button>
+
+            {error && <p>{error.message}</p>}
           </form>
         </div>
       </div>
@@ -26,4 +88,11 @@ render() {
   }
 }
 
+const LoginForm = compose(
+  withRouter,
+  withFirebase,
+)(LoginFormBase);
+
 export default Login;
+
+export { LoginForm };
