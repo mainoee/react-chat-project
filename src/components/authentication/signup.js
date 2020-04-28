77b09e1f7265ddb4e8fa3d16cf4fe4897d6a3323
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-28 17:25:25 +0200

    install recompose and add to Signup page

--- src/components/authentication/signup.js
+++ src/components/authentication/signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../../service';
 
@@ -108,7 +109,10 @@ class SignUpFormBase extends Component {
   }
 }
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 export default Signup;
 
