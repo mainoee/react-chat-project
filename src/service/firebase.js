698de52fdc48d29141159970e48e11f265ffd1aa
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-04-28 16:19:47 +0200

    implement authentication API for Firebase class

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,6 +15,8 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
 }
 
