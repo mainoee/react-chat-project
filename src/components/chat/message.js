470b5686b9c1e4de5f5a61edd83b6757e1cc2a01
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-01 22:54:06 +0200

    update Message component to rend message content

--- src/components/chat/message.js
+++ src/components/chat/message.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const Message = (props) => {
-  return (
-    <div className="message-container">
-      <i>
-        <span>{props.message.username}</span>
-        <small>{props.message.time}</small>
-        <p>{props.message.content}</p>
-      </i>
-    </div>
-  );
-};
-
+const Message = ({ message }) => (
+  <div className="message-container">
+    <i>
+      <span>{message.userId}</span>
+      <p>{message.text}</p>
+    </i>
+  </div>
+);
 
 export default Message;
