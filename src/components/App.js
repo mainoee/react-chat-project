5b22aac498d38959a6af4a517b8dea0e6317eb22
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-01 16:49:49 +0200

    move authentication logic from App to new withAuthentication component

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,10 @@ import Signup from './authentication/signup';
 import PrivateRoute from './authentication/privateRoute';
 import Navigation from './Navigation';
 
-import { withFirebase } from '../service';
-import { AuthUserContext } from './Session';
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      authUser: null,
       channels: ["Politics", "Economics", "World"],
       messages: [
         {
@@ -32,41 +28,26 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(
-      authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
   render() {
     return (
-      <AuthUserContext.Provider value={this.state.authUser}>
-        <div>
-          <Router>
-            <Navigation />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <PrivateRoute path="/chat"
-                render={(props) => <Chat {...props}
-                                         channels={this.state.channels}
-                                         messages={this.state.messages} />}
-              />
-            </Switch>
-          </Router>
-        </div>
-      </AuthUserContext.Provider>
+      <div>
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <PrivateRoute path="/chat"
+              render={(props) => <Chat {...props}
+                                       channels={this.state.channels}
+                                       messages={this.state.messages} />}
+            />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
 
-export default withFirebase(App);
+export default App;
 
