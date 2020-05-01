85626b8448b1acf08fec0e6c1d0f895ea37f1020
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-01 22:35:11 +0200

    render in Chat component list of messages stored in Firebase database

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import MessagesList from './messagesList';
-import Channels from './channels';
+// import Channels from './channels';
+
+import { withFirebase } from '../../service';
 
 import { AuthUserContext, withAuthorization } from '../Session';
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.messages().on('value', snapshot => {
+      this.setState({ loading: false });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.messages().off();
+  }
+
   render() {
+    const { messages, loading } = this.state;
     return (
       <AuthUserContext.Consumer>
         {authUser => (
           <div className="messaging-wrapper">
-            <Channels channels={this.props.channels} />
-            <MessagesList messages={this.props.messages} />
+            {loading && <div>Loading ...</div>}
+            <MessagesList messages={messages} />
           </div>
         )}
       </AuthUserContext.Consumer>
