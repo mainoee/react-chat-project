b8b6773b2daabc5f32f9031e89f1d957248bff96
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-01 22:28:16 +0200

    remove fake data from App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from './home';
@@ -9,46 +9,17 @@ import Navigation from './Navigation';
 
 import { withAuthentication } from './Session';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      channels: ["Politics", "Economics", "World"],
-      messages: [
-        {
-          time: new Date().toLocaleTimeString(),
-          username: 'mainoee',
-          content: 'Welcome to the chat!'
-        },
-        {
-          time: new Date().toLocaleTimeString(),
-          username: 'jcameron',
-          content: 'Hi there!'
-        },
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navigation />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/chat"
-              render={(props) => <Chat {...props}
-                                       channels={this.state.channels}
-                                       messages={this.state.messages} />}
-            />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Navigation />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/chat" component={Chat} />
+    </Switch>
+  </Router>
+);
 
 export default withAuthentication(App);
 
