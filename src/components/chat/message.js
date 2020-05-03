50e9cf8353fa361fa3ab907bef1d557b1864d9c2
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-03 09:42:18 +0200

    display username instead of message.userId in Message component

--- src/components/chat/message.js
+++ src/components/chat/message.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
+import { withFirebase } from '../../service';
+
 class Message extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+  }
+
+  componentDidMount() {
+    const authUser = this.props.message.userId
+
+    this.props.firebase.user(authUser).on('value', snapshot => {
+      const user = snapshot.val();
+
+      if (user) {
+        this.setState({ username: user.username });
+      } else {
+        this.setState({ username: '' });
+      }
+    });
+  }
+
   render() {
     const { message } = this.props;
     const date = new Date(message.createdAt).toLocaleTimeString();
     return (
       <div>
-        <span>{message.userId}</span><p>{date}</p>
+        <span>{this.state.username}</span> <i>{date}</i>
         <p>{message.content}</p>
       </div>
     )
   }
 }
 
-export default Message;
+export default withFirebase(Message);
