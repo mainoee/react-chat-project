0b9b822b28a14f813cb7cafbeea40cc830aef92a
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-09 11:55:34 +0200

    set en/fr translation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { IntlProvider } from "react-intl";
+import translations from "./i18n/locales";
 
 import './assets/stylesheets/index.css';
 import './assets/stylesheets/navbar.css';
@@ -10,10 +11,17 @@ import './assets/stylesheets/authentication.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/App';
+
 import Firebase, { FirebaseContext } from './service';
 
+const locale = navigator.language.slice(0,2);
+
+const translationsHasLocale = translations.hasOwnProperty(locale);
+
+const messages = (translationsHasLocale) ? translations[locale] : translations.en;
+
 ReactDOM.render(
-  <IntlProvider>
+  <IntlProvider locale={locale} messages={messages}>
     <FirebaseContext.Provider value={new Firebase()}>
       <App />
     </FirebaseContext.Provider>
