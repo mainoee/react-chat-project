94256ee4967f5d23b75c2c099ec81e71d8004e3f
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-11 09:07:14 +0200

    set fr/en translation for send button in MessageForm component

--- src/components/chat/MessageForm.js
+++ src/components/chat/MessageForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FormattedMessage } from 'react-intl';
+
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -13,6 +15,7 @@ const sendButton = makeStyles((theme) => ({
 const MessageForm = (props) => {
   const classes = sendButton();
   const gif = props.selectedGif
+
   return(
     <form onSubmit={(event) => props.onCreateMessage(event, props.authUser)} className="message-form">
       <input
@@ -21,13 +24,17 @@ const MessageForm = (props) => {
         onChange={props.onChangeContent}
       />
       <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          className={classes.root}
-          labelstyle={{ fontSize: '10px' }}
-          disabled={!gif && !props.content}
-        >Send
+        type="submit"
+        variant="contained"
+        color="primary"
+        className={classes.root}
+        labelstyle={{ fontSize: '10px' }}
+        disabled={!gif && !props.content}
+      >
+        <FormattedMessage
+          id="Button.send.messageForm"
+          defaultMessage="Send"
+        />
       </Button>
     </form>
   )
